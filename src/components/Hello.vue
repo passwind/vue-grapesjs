5bc865ae1ade28109b3e97b8f956072f111e358c
Zhu Yu <mike.zhuyu@gmail.com>
2017-08-10 19:01:51 +0800

    Init repo

---
+++ src/components/Hello.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <div id="gjs"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted () {
+    var editor = grapesjs.init({
+      container: '#gjs',
+      components: '<div class="txt-red">Hello world!</div>',
+      style: '.txt-red{color: red}'
+    })
+    console.log(editor)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/includes.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-undef */
+window.$ = window.jQuery = require('jquery')
+require('../static/vendors/grapes/css/grapes.min.css')
+window.grapesjs = require('../static/vendors/grapes/grapes.min.js')
